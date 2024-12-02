63c82a3cafbb9698c3193ac03f5fd04fb78685d8
Daniël de Kok <me@danieldk.eu>
2024-12-02 16:30:44 +0000

    Extensions as a map + build without rpath for outside Nix
    This is still a bit of a mess, but saving for the end of the day.

--- lib/build.nix
+++ lib/build.nix
@@ -25,16 +25,36 @@ rec {
 
   buildTorchExtension =
     path:
+    torch:
     let
       buildConfig = readBuildConfig path;
       extConfig = buildConfig.extension;
     in
     pkgs.callPackage ./torch-extension {
+      inherit torch;
       extensionName = extConfig.name;
       extensionSources = extConfig.src;
       kernels = buildKernels path;
       src = path;
-      torch = pkgs.python3Packages.torch_2_4;
     };
- 
+
+    buildNixTorchExtensions = let
+      # TODO: add other Torch and CUDA versions.
+      torchVersions = with pkgs.python3Packages; [torch_2_4 ];
+      flattenVersion = version: ''torch${lib.replaceStrings ["."] [""] (lib.versions.pad 2 version)}'';
+      extensionForTorch = path: torch: { name = flattenVersion torch.version; value = buildTorchExtension path torch; };
+    in
+      path: builtins.listToAttrs (lib.map (extensionForTorch path) torchVersions);
+
+    # TODO: rewrite rpaths.
+    buildTorchExtensions = let
+      stripRPath = drv: pkgs.runCommand "without-rpath" {} ''
+        mkdir -p $out/lib
+        find ${drv}/lib -name '*.so' -exec cp --no-preserve=mode {} $out/lib \;
+
+        find $out/lib -name '*.so' \
+          -exec patchelf --set-rpath '/opt/hostedtoolcache/Python/3.11.9/x64/lib' {} \;
+      '';
+    in
+      path: lib.mapAttrs (v: drv: stripRPath drv) (buildNixTorchExtensions path);
 }
