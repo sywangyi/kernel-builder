11e3dfbf2d2c97fcee52affe8aebb93a6e87253d
Daniël de Kok <me@danieldk.eu>
2024-12-06 14:41:50 +0000

    Add activation kernels as an example

---
+++ examples/activation/README.md
@@ -0,0 +1,5 @@
+## Activation
+
+Activation kernels from [vLLM](https://github.com/vllm-project/vllm/blob/main/csrc/activation_kernels.cu).
+
+Copyright 2023-2024, the vLLM team.
---
+++ examples/activation/ext-torch/torch_binding.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <torch/torch.h>
+
+void silu_and_mul(torch::Tensor &out, torch::Tensor &input);
+
+void gelu_and_mul(torch::Tensor &out, torch::Tensor &input);
+
+void gelu_tanh_and_mul(torch::Tensor &out, torch::Tensor &input);
+
+void fatrelu_and_mul(torch::Tensor &out, torch::Tensor &input,
+                     double threshold);
+
+void gelu_new(torch::Tensor &out, torch::Tensor &input);
+
+void gelu_fast(torch::Tensor &out, torch::Tensor &input);
+
+void gelu_quick(torch::Tensor &out, torch::Tensor &input);
