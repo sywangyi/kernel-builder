5e48ab9f36a5ec14bee92abca2bb0c8ba979e39a
Daniël de Kok <me@danieldk.eu>
2025-05-28 12:31:24 +0200

    Provide better errors when deserializing `build.toml` (#145)
    Before this change when there was an error in `build.toml`, one would
    get a generic error:
    
    ```
    Error: Cannot parse TOML in /Users/daniel/git/relu-metal/build.toml
    
    Caused by:
        data did not match any variant of untagged enum BuildCompat
    ```
    
    However, what we want is to give the error when trying to parse as the
    V2 format, since the V1 format is only there for backwards
    compat/updates. With this change the error of parsing as V2 is bubbled
    up. For instance:
    
    ```
    Error: Cannot parse TOML in /Users/daniel/git/relu-metal/build.toml
    
    Caused by:
        Unknown variant metal. Expected one of cuda, rocm
    ```

--- build2cmake/src/config/mod.rs
+++ build2cmake/src/config/mod.rs
@@ -4,13 +4,31 @@ use serde::Deserialize;
 pub mod v1;
 
 mod v2;
+use serde_value::Value;
 pub use v2::{Backend, Build, Dependencies, Kernel, Torch};
 
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
+#[derive(Debug)]
 pub enum BuildCompat {
     V1(v1::Build),
     V2(Build),
+}
+
+impl<'de> Deserialize<'de> for BuildCompat {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let value = Value::deserialize(deserializer)?;
+
+        match v1::Build::deserialize(value.clone()) {
+            Ok(v1_build) => Ok(BuildCompat::V1(v1_build)),
+            Err(_) => {
+                let v2_build: Build =
+                    Build::deserialize(value).map_err(serde::de::Error::custom)?;
+                Ok(BuildCompat::V2(v2_build))
+            }
+        }
+    }
 }
 
 impl TryFrom<BuildCompat> for Build {
