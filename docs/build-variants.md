10955f0b5a30e6a81df371914534b6931fa80578
Daniël de Kok <me@danieldk.eu>
2025-09-12 11:27:42 +0000

    Remove all Torch 2.7 variants

--- docs/build-variants.md
+++ docs/build-variants.md
@@ -7,32 +7,25 @@ available. This list will be updated as new PyTorch versions are released.
 
 ## Metal aarch64-darwin
 
-- `torch27-metal-aarch64-darwin`
 - `torch28-metal-aarch64-darwin`
 
 ## CUDA aarch64-linux
 
-- `torch27-cxx11-cu128-aarch64-linux`
 - `torch28-cxx11-cu129-aarch64-linux`
 
 ## CUDA x86_64-linux
 
-- `torch27-cxx11-cu118-x86_64-linux`
-- `torch27-cxx11-cu126-x86_64-linux`
-- `torch27-cxx11-cu128-x86_64-linux`
 - `torch28-cxx11-cu126-x86_64-linux`
 - `torch28-cxx11-cu128-x86_64-linux`
 - `torch28-cxx11-cu129-x86_64-linux`
 
 ## ROCm x86_64-linux
 
-- `torch27-cxx11-rocm63-x86_64-linux`
 - `torch28-cxx11-rocm63-x86_64-linux`
 - `torch28-cxx11-rocm64-x86_64-linux`
 
 ## XPU x86_64-linux
 
-- `torch27-cxx11-xpu20250-x86_64-linux`
 - `torch28-cxx11-xpu20251-x86_64-linux`
 
 ## Universal
--- versions.nix
+++ versions.nix
@@ -1,67 +1,11 @@
 [
   {
-    torchVersion = "2.7";
-    cudaVersion = "11.8";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    cudaVersion = "12.6";
-    cxx11Abi = true;
-    systems = [
-      "x86_64-linux"
-    ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    cudaVersion = "12.8";
-    cxx11Abi = true;
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    rocmVersion = "6.3.4";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
-    torchVersion = "2.7";
-    rocmVersion = "6.4.2";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = false;
-  }
-
-  {
-    torchVersion = "2.7";
-    xpuVersion = "2025.0.2";
-    cxx11Abi = true;
-    systems = [ "x86_64-linux" ];
-    bundleBuild = true;
-  }
-  {
     torchVersion = "2.8";
     xpuVersion = "2025.1.3";
     cxx11Abi = true;
     systems = [ "x86_64-linux" ];
     bundleBuild = true;
   }
-  {
-    torchVersion = "2.7";
-    cxx11Abi = true;
-    metal = true;
-    systems = [ "aarch64-darwin" ];
-    bundleBuild = true;
-  }
-
   {
     torchVersion = "2.8";
     cudaVersion = "12.6";
