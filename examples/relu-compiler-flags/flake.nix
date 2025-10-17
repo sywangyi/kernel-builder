06793ac59412869e03032d2b344ec3705a5f92bb
Daniël de Kok <me@danieldk.eu>
2025-10-17 08:36:28 +0200

    Check that `cuda-flags`/`sycl-flags` are properly passed (#264)

---
+++ examples/relu-compiler-flags/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Flake for ReLU kernel";
+
+  inputs = {
+    kernel-builder.url = "path:../..";
+  };
+
+  outputs =
+    {
+      self,
+      kernel-builder,
+    }:
+    kernel-builder.lib.genFlakeOutputs {
+      inherit self;
+      path = ./.;
+    };
+}
