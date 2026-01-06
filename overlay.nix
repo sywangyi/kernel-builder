03fc4c6a2b88319ddd3dfcd01142a398975b1e79
Daniël de Kok <me@danieldk.eu>
2026-01-06 13:50:00 +0100

    Bump up kernels dependency to 0.11.5 (#338)
    This should fix uploads of noarch kernels.

--- overlay.nix
+++ overlay.nix
@@ -74,14 +74,14 @@ in
 
         kernel-abi-check = callPackage ./pkgs/python-modules/kernel-abi-check { };
 
-        kernels = python-super.kernels.overrideAttrs (oldAttrs: {
-          version = "unstable";
+        kernels = python-super.kernels.overrideAttrs (oldAttrs: rec {
+          version = "0.11.5";
 
           src = final.fetchFromGitHub {
             owner = "huggingface";
             repo = "kernels";
-            rev = "0e18dbf076fc44de5dac4027616e9f3d9e2da45a";
-            sha256 = "sha256-6N1W3jLQIS1yEAdNR2X9CuFdMw4Ia0yzBBVQ4Kujv8U=";
+            tag = "v${version}";
+            sha256 = "sha256-nPb0MvH3bvxNo64JkhhmrfI8YpSTxQif1+Pk35ywKDI=";
           };
         });
 
